19a57028a8ab5dd071717dba8e45a80c55b3e615
Vetri-6401 <142138725+Vetri-6401@users.noreply.github.com>
2024-04-15 14:31:39 +0530

    Delete chatbot/Fnet directory

--- chatbot/Fnet/chatbot/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.open_chat,name='Home'),
-]
--- chatbot/Fnet/chatbot/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def open_chat(request):
-
-    return render(request,'index.html')
